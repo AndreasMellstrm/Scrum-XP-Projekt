b8d53b76c683728ef201cbb74eee1df8b97dca49
åsa <aasa.haakansson@gmail.com>
2019-01-29 13:50:07 +0100

    Fixat så det går att ladda upp filer

--- Örebro Universitet Kommunikation/Örebro Universitet Kommunikation/Controllers/FormalBlogController.cs
+++ Örebro Universitet Kommunikation/Örebro Universitet Kommunikation/Controllers/FormalBlogController.cs
@@ -1,5 +1,9 @@
 ﻿using Örebro_Universitet_Kommunikation.Models;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Örebro_Universitet_Kommunikation.Controllers {
@@ -27,7 +31,41 @@ namespace Örebro_Universitet_Kommunikation.Controllers {
 
             });
         }
+        
+        public string FileUpload(HttpPostedFileBase File)
+        {
 
+            //Vi kollar att det finns en fil att spara
+            if (File != null && File.ContentLength > 0)
+            {
+                //Hämtar filnamnet utan filändelse
+                var NoExtension = Path.GetFileNameWithoutExtension(File.FileName);
+                //Hämtar filändelsen
+                var Extension = Path.GetExtension(File.FileName);
+                //Lägger ihop de båda, men med nuvarande tidpukt i namnet.
+                //Det görs för att filnamnet ska bli unikt.
+                var NameOfFile = NoExtension + DateTime.Now.ToString("yyyy-MM-dd-fff") + Extension;
+                //Filen där filerna sparas och filnamnet slås ihop till en sträng
+                var NameOfPath = "/Content/Files/" + NameOfFile;
+                string FilePath = Path.Combine(Server.MapPath("~/Content/Files/"), NameOfFile);
+                //Filen sparas
+                File.SaveAs(FilePath);
+
+                return NameOfPath;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        public ActionResult TempUpload(HttpPostedFileBase File)
+        {
+            
+            var fileString = FileUpload(File);
+            Debug.WriteLine(fileString);
+            return View();
+        }
         
     }
 }
