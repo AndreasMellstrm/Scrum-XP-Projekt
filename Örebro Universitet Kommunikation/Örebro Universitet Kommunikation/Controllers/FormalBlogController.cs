cd418078e910c8372695067032967ef5ee6a051a
André Persson <knodden@live.se>
2019-01-30 09:35:43 +0100

    Fix

--- Örebro Universitet Kommunikation/Örebro Universitet Kommunikation/Controllers/FormalBlogController.cs
+++ Örebro Universitet Kommunikation/Örebro Universitet Kommunikation/Controllers/FormalBlogController.cs
@@ -41,7 +41,6 @@ namespace Örebro_Universitet_Kommunikation.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult CreateEntry(FormalBlogEntry model, HttpPostedFileBase File) {
-            isCreated = false;
             var user = UserManager.FindById(User.Identity.GetUserId());
             var fileString = FileUpload(File);
             Ctx.FormalBlogEntreis.Add(new FormalBlogEntry {
