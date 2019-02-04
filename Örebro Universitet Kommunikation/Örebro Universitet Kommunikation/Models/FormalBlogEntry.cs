8a09a46caadcce32be5097eb5bdc8c87d25ac51d
André Persson <knodden@live.se>
2019-02-04 14:06:04 +0100

    LIte design fix på kommentarsidan.

--- Örebro Universitet Kommunikation/Örebro Universitet Kommunikation/Models/FormalBlogEntry.cs
+++ Örebro Universitet Kommunikation/Örebro Universitet Kommunikation/Models/FormalBlogEntry.cs
@@ -10,7 +10,7 @@ namespace Örebro_Universitet_Kommunikation.Models {
         public int Id { get; set; }
         public string CreatorId { get; set; }
         [Required]
-        [StringLength(100, ErrorMessage = "Din {0} måste vara minst {2} tecken.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Din {0} måste vara minst {2} tecken.", MinimumLength = 3)]
         [Display(Name = "Titel")]
         public string Title { get; set; }
         [Display(Name = "Inlägg")]
