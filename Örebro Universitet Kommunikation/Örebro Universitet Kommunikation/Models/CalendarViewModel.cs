ffc6deceda0ce58792fa003d3289bb2206e07351
ludvig lundh <l.lundh@hotmail.com>
2019-02-06 09:37:11 +0100

    nu syns kalender under Kalender i navbar, ligger exempeldatum inlagd som hämtas från  databas

---
+++ Örebro Universitet Kommunikation/Örebro Universitet Kommunikation/Models/CalendarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Örebro_Universitet_Kommunikation.Models {
+    public class CalendarViewModel {
+        public int Sr { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public string Start_Date { get; set; }
+        public string End_Date { get; set; }
+    }
+}
